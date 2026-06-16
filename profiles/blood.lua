ffd5561ddd5a04af4963a5be6642573c5b1ad702
Kyle Ratliff <kylecrat@me.com>
2026-06-16 05:00:04 -0400

    add blood and nature profiles

---
+++ profiles/blood.lua
@@ -0,0 +1,80 @@
+{
+    accents = {
+        color = {
+            a = 1,
+            b = 0,
+            g = 0,
+            r = 0.78431379795074,
+        },
+    },
+    frame = {
+        background_color = {
+            a = 1,
+            b = 0.019607843831182,
+            g = 0,
+            r = 0.12549020349979,
+        },
+        height = 38,
+        width = 1100,
+    },
+    health = {
+        font = {
+            color = {
+                a = 1,
+                b = 1,
+                g = 1,
+                r = 1,
+            },
+            font = "Interface\\AddOns\\ElvUI\\Game\\Shared\\Media\\Fonts\\DieDieDie.ttf",
+            offset = {
+                y = -4,
+            },
+            size = 26,
+        },
+        percent_font = {
+            disable_above = 2,
+            offset = {
+                x = -33,
+            },
+        },
+        spark = {
+            atlas = "GarrMission_EncounterBar-Spark",
+            blend_mode = "ADD",
+            color = {
+                a = 1,
+                b = 0.4588235616684,
+                g = 0.47843140363693,
+                r = 1,
+            },
+            width = 10,
+        },
+        texture = {
+            atlas = true,
+            atlas_texture = "Unit_DemonHunter_Fury_Fill",
+            color = {
+                a = 1,
+                b = 0,
+                g = 0,
+                r = 0.78431379795074,
+            },
+            desaturated = true,
+            texture = false,
+        },
+    },
+    name = {
+        font = {
+            color = {
+                a = 1,
+                b = 0,
+                g = 0,
+                r = 0.78431379795074,
+            },
+            font = "Interface\\AddOns\\ElvUI\\Game\\Shared\\Media\\Fonts\\DieDieDie.ttf",
+            size = 32,
+        },
+        offset = {
+            x = 4,
+            y = 6,
+        },
+    },
+}
